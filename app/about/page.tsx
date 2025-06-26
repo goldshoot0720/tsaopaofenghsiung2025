b2957c5b0b97d95986ac7a4c249c3a8a02c2a6dc
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-26 17:32:33 +0800

    Thank to 202507050418.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,30 +3,16 @@ import { title } from "@/components/primitives";
 export default function AboutPage() {
   return (
     <>
-    <div>
-      <h1 className={title()}>關於</h1>
-    </div>
-    <h1>
-      Thank to Vercel
-    </h1>
-    <h1>
-      Thank to Next.js
-    </h1>
-    <h1>
-      Thank to HeroUI
-    </h1>
-    <h1>
-      Thank to React
-    </h1>
-    <h1>
-      Thank to ChatGPT
-    </h1>
-     <h1>
-    Thank to Google(Text-to-Speech AI)
-    </h1>
-    <h1>
-      Thank to Font Awesome
-    </h1>
+      <div>
+        <h1 className={title()}>關於</h1>
+      </div>
+      <h1>Thank to Vercel/Netlify</h1>
+      <h1>Thank to Next.js</h1>
+      <h1>Thank to HeroUI</h1>
+      <h1>Thank to React</h1>
+      <h1>Thank to ChatGPT</h1>
+      <h1>Thank to Google(Text-to-Speech AI)</h1>
+      <h1>Thank to Font Awesome</h1>
     </>
   );
 }
