9a009be64bb7c8dc950c044f58bbf1deb879fab5
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-28 23:16:58 +0800

    Update 3 Thank to 202507050418.

---
+++ tsaopaofenghsiung2025-git.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\tsaopaofenghsiung2025"
+git init
+git add .
+git commit -m "Update 3 Thank to 202507050418."
+git remote add origin https://github.com/goldshoot0720/tsaopaofenghsiung2025.git
+git checkout -b master
+git push -u origin master --force
+pause
