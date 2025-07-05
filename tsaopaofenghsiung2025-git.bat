99aaac4ce14947d83b711aac36eb4ef7c0368854
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-06 01:10:45 +0800

    tpe 20250706 delay 3

--- tsaopaofenghsiung2025-git.bat
+++ tsaopaofenghsiung2025-git.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\tsaopaofenghsiung2025"
 git init
 git add .
-git commit -m "tpe 20250706 delay"
+git commit -m "tpe 20250706 delay 3"
 git remote add origin https://github.com/goldshoot0720/tsaopaofenghsiung2025.git
 git checkout -b master
 git push -u origin master --force
