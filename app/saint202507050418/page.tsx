84cc96f1a676a47ccd32f72d4e92994b12aa3a33
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-11 14:51:19 +0800

    20250711 v1 plyr

--- app/saint202507050418/page.tsx
+++ app/saint202507050418/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import Image from "next/image";
 import { title } from "@/components/primitives";
-
+import { useEffect } from "react";
+import Plyr from "plyr";
+import "plyr/dist/plyr.css";
 export default function Saint202507050418Page() {
+  useEffect(() => {
+    const player = new Plyr("#player");
+  }, []);
   return (
     <>
       <h1 className={title()}>第8屆回顧</h1>
@@ -36,7 +42,7 @@ export default function Saint202507050418Page() {
         title="30分鐘完整版／自稱繼承人！唐新民扯「馬英九毒殺蔣經國」哽咽：死得好冤｜三立新聞網 SETN.com"
         width={333}
       />
-      <video width="333" height="333" controls>
+      <video id="player" width="333" height="333" controls>
         <source
           src="https://wrn5xhmwg3rulhoy.public.blob.vercel-storage.com/30%E5%88%86%E9%90%98%E5%AE%8C%E6%95%B4%E7%89%88%EF%BC%8F%E8%87%AA%E7%A8%B1%E7%B9%BC%E6%89%BF%E4%BA%BA%EF%BC%81%E5%94%90%E6%96%B0%E6%B0%91%E6%89%AF%E3%80%8C%E9%A6%AC%E8%8B%B1%E4%B9%9D%E6%AF%92%E6%AE%BA%E8%94%A3%E7%B6%93%E5%9C%8B%E3%80%8D%E5%93%BD%E5%92%BD%EF%BC%9A%E6%AD%BB%E5%BE%97%E5%A5%BD%E5%86%A4%EF%BD%9C%E4%B8%89%E7%AB%8B%E6%96%B0%E8%81%9E%E7%B6%B2%20SETN_com-40c0087e65e7362b-pKT6bzP3T3ToF87AX0YIBtjUwx3Ap8.mp4"
           type="video/mp4"
--- tsaopaofenghsiung2025-git.bat
+++ tsaopaofenghsiung2025-git.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\tsaopaofenghsiung2025"
 git init
 git add .
-git commit -m "tpe 20250708 20250709 me no1"
+git commit -m "20250711 v1 plyr"
 git remote add origin https://github.com/goldshoot0720/tsaopaofenghsiung2025.git
 git checkout -b master
 git push -u origin master --force
